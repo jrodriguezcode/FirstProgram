86cfaa0b9ca07a85344987acce63ca808662c817
jrodriguezcode <jr698751@gmail.com>
2025-01-27 12:29:28 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		System.out.print("I love rice and beans");
+//		System.out.print("It's really good!");
+		
+		System.out.println("I love rice and beans");
+		System.out.println("It's really good!");
+		
+//		System.out.print("This is for an escape sequence\n");
+//		System.out.print("It's for testing purposes.");
+		
+//		System.out.println("Escape sequence with println\n");
+//		System.out.println("And here is the text below it.");
+		
+//		System.out.println("\t Testing out the tab function");
+		
+//		System.out.println("\"Testing out putting quotes\"");
+		
+//		System.out.println("\\ You need 2 backslashes to show 1");
+		
+		// This comment is ignored by the compiler
+		
+		/*
+		 * This
+		 * is
+		 * a
+		 * comment
+		 */
+		
+	}
+
+}
